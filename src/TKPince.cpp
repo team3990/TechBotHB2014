25a8cbcce32abd80be2c66e62746383593ff008f
InsaneBeaver <booyeah@hotmail.fr>
2014-11-22 14:43:51 -0500

    TKPince class added

---
+++ src/TKPince.cpp
@@ -0,0 +1,18 @@
+/*
+ * TKPince.cpp
+ *
+ *  Created on: 2014-11-22
+ *      Author: 11110305
+ */
+
+#include "TKPince.h"
+
+TKPince::TKPince() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TKPince::~TKPince() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TKPince.h
@@ -0,0 +1,17 @@
+/*
+ * TKPince.h
+ *
+ *  Created on: 2014-11-22
+ *      Author: 11110305
+ */
+
+#ifndef SRC_TKPINCE_H_
+#define SRC_TKPINCE_H_
+
+class TKPince {
+public:
+	TKPince();
+	virtual ~TKPince();
+};
+
+#endif /* SRC_TKPINCE_H_ */
